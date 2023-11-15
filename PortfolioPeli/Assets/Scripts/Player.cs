fdd42f2a77e0eff01c02b60d8270670550e0bee4
AleksiHeikk <alekhei@metropolia.fi>
2023-11-15 16:18:18 +0200

    update

--- PortfolioPeli/Assets/Scripts/Player.cs
+++ PortfolioPeli/Assets/Scripts/Player.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float playerScore = 0;
+
     public float moveSpeed = 5.0f;
 
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform m_bulletSpawn;
+
+    private Animator anim;
 
     public void Shoot()
     {
@@ -19,6 +23,7 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         Vector3 movement = new Vector3(horizontalInput, 0, 0) * moveSpeed * Time.deltaTime;
         transform.Translate(movement, Space.World);
+
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
